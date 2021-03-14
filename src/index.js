8b70ab4fc2f6139d32e3aae47ed7c657cd611fee
haticed <77358594+haticed@users.noreply.github.com>
2021-03-14 21:32:09 +0000

    React project started

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1- import the library we use 
+import React from 'react' 
+// to import reat for the react library 
+import ReactDom from 'react-dom' 
+// import reactdom to see from the screen 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//2- create a component " there is two way to create a companat Class or Function "
+const App = () =>{
+    return (
+        <div>
+            <h1> Hi React!</h1>
+        </div>
+    )
+}
+//3- to see as a web page you need to export it 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reactDom.render(<element/>, Dommanipulation)
+ReactDom.render (<App/>, document.querySelector("#root"))
